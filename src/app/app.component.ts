ef66abcf3f25c76108bbd5d7874b12435f6ce752
Mete Aydin <meteaydin050@gmail.com>
2020-10-06 09:57:21 +0300

    dashboards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
+import { Task } from './models/task';
+import { Product } from './models/product';
 import { Component } from '@angular/core';
+import {LazyLoadEvent} from 'primeng/api';
+
+
+import tdata from './resources/tasks.json';
+import sdata from './resources/stocks.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'charts';
+  stocks:Product[];
+  totalRecords:number;
+  loading:boolean;
+  cols: any[];
+  products:Product[];
+
+  tasks:Task[];
+  totalTasks:number;
+
+  
+  data: any;
+  data2: any;
+  data3: any;
+  
+  newcustomer: number;
+  allbills: number;
+  allproducts: number;
+
+  constructor() {
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Gelir',
+          backgroundColor: '#9CCC65',
+          borderColor: '#7CB342',
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+        {
+          label: 'Gider',
+          backgroundColor: '#ff6a6a',
+          borderColor: '#ee6363',
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+      ],
+    };
+    this.data2 = {
+      labels: ['Nakit', 'Kredi Kartı', 'Mail-Order'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        },
+      ],
+    };
+    this.newcustomer = 27;
+    this.allproducts = 1560;
+    this.allbills = 64;
+
+    this.data3 = {
+      labels: ['Klima', 'Kombi', 'Hava Temizleyici'],
+      datasets: [
+        {
+          data: [40, 50, 100],
+          backgroundColor: ['#8e0101', '#0a5400', '#ff7f00'],
+          hoverBackgroundColor: ['#FF6384', '#047a18', '#FFCE56'],
+        },
+      ],
+    };
+  }
+  ngOnInit(): void {
+    this.stocks=sdata;
+    this.totalRecords=sdata.length;
+    this.loading=true;
+
+    this.tasks=tdata;
+    this.totalTasks=tdata.length;
+
+  }
+  loadStocks(event: LazyLoadEvent){
+    this.loading=true;
+    event.rows=3;
+
+    setTimeout(() => {
+      if (this.stocks) {
+          this.products = this.stocks.slice(event.first, (event.first + event.rows));
+          this.loading = false;
+      }
+  }, 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
+import { FullCalendarModule } from 'primeng/fullcalendar';
+import { ChartModule } from 'primeng/chart';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import { OrderListModule } from 'primeng/orderlist';
+import { TabViewModule } from 'primeng/tabview';
+import { CheckboxModule } from 'primeng/checkbox';
+import {CarouselModule} from 'primeng/carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +23,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartModule,
+    CardModule,
+    TableModule,
+    MessagesModule,
+    MessageModule,
+    OrderListModule,
+    TabViewModule,
+    CheckboxModule,
+    ButtonModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
